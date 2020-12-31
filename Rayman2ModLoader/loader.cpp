153bef2a8eadec87c08cbf01684c0f9151f0830a
kellsnc <kellspam@outlook.fr>
2020-12-31 19:47:37 +0100

    Remove game config override

--- Rayman2ModLoader/loader.cpp
+++ Rayman2ModLoader/loader.cpp
@@ -13,16 +13,6 @@ std::wstring ModsPath = L"Mods\\";
 std::string DLLName = "GliVd1";
 std::string APIName = "Glide2";
 
-GameConfig_t GameConfig = {
-    /*   ScreenWidth   */ 1920,
-    /*   ScreenHeight  */ 1080,
-    /*   ScreenDepth   */ 32,
-    /*   ParticuleRate */ 3.0f,
-    /*   TexturesMem   */ 6,
-    /*   SoundHD       */ true,
-    /*   Complete      */ false
-};
-
 void SplitFilename(std::wstring* str) {
     std::string::size_type slash = str->find_last_of(L"/\\");
     *str = str->substr(0, slash);
@@ -57,41 +47,15 @@ void GetConfigPath() {
     ConfigPath = ConfigPath;
 }
 
-void ApplyGameConfigs() {
-    OptionScreenWidth = GameConfig.ScreenWidth;
-    OptionScreenHeight = GameConfig.ScreenHeight;
-    OptionScreenDepth = GameConfig.ScreenDepth;
-    OptionParticuleRate = GameConfig.ParticuleRate;
-    OptionTexturesMem = GameConfig.TexturesMem;
-}
-
-void ReadGameConfig(const IniGroup* gameconfig) {
-    if (gameconfig) {
-        GameConfig.ScreenWidth = gameconfig->getInt("ScreenWidth", GameConfig.ScreenWidth);
-        GameConfig.ScreenHeight = gameconfig->getInt("ScreenHeight", GameConfig.ScreenHeight);
-        GameConfig.ScreenDepth = gameconfig->getInt("ScreenDepth", GameConfig.ScreenDepth);
-        GameConfig.ParticuleRate = gameconfig->getFloat("ParticuleRate", GameConfig.ParticuleRate);
-        GameConfig.TexturesMem = gameconfig->getInt("TexturesMem", GameConfig.TexturesMem);
-        GameConfig.SoundHD = gameconfig->getBool("SoundHD", GameConfig.SoundHD);
-        GameConfig.Complete = gameconfig->getBool("SoundHD", GameConfig.Complete);
-    }
-    
-    ApplyGameConfigs();
-}
-
 void ReadConfig() {
     const IniFile* config = new IniFile(ConfigPath);
-    const IniGroup* loaderconfig = config->getGroup("ModLoader");
+    const IniGroup* loaderconfig = config->getGroup("Rayman2ModLoader");
 
     if (loaderconfig) {
         DLLName = loaderconfig->getString("GlideDLL", DLLName);
         APIName = loaderconfig->getString("GlideAPI", APIName);
         ModsPath = loaderconfig->getWString("ModsPath", ModsPath);
         CodesPath = loaderconfig->getWString("CodesPath", CodesPath);
-
-        if (loaderconfig->getBool("OverrideConfig", true)) {
-            ReadGameConfig(config->getGroup("Rayman2"));
-        }
 
         // Load codes
 
@@ -122,10 +86,7 @@ void ReadConfig() {
             L"Using default configuration with no mod/code enabled.",
             L"Rayman2 Mod Loader", MB_OKCANCEL | MB_ICONERROR);
 
-        if (type == IDOK) {
-            ApplyGameConfigs();
-        }
-        else {
+        if (type != IDOK) {
             ExitProcess(1);
         }
     }
@@ -137,4 +98,6 @@ void LoaderInit() {
     GetGamePath();
     GetConfigPath();
     ReadConfig();
+
+    ExitProcess(1);
 }
--- Rayman2ModLoader/loader.h
+++ Rayman2ModLoader/loader.h
@@ -1,19 +1,6 @@
 #pragma once
 
-struct GameConfig_t {
-	int ScreenWidth;
-	int ScreenHeight;
-	int ScreenDepth;
-	float ParticuleRate;
-	int TexturesMem;
-
-	// Unused
-	bool SoundHD;
-	bool Complete;
-};
-
 extern std::string DLLName;
 extern std::string APIName;
-extern GameConfig_t GameConfig;
 
 void LoaderInit();
