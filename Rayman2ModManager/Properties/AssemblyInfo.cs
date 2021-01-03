79f4796850d235f78aef279a8633a4a3d06798a3
kellsnc <kellspam@outlook.fr>
2021-01-03 22:24:49 +0100

    Add ModManagerCommon

--- Rayman2ModManager/Properties/AssemblyInfo.cs
+++ Rayman2ModManager/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rayman2ModManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Manages mod to use with Rayman2ModLoader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rayman2ModManager")]
