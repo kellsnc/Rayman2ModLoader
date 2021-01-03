10584f677824c9492bb6fff9256a99c2d0f25e04
kellsnc <kellspam@outlook.fr>
2021-01-03 22:21:29 +0100

    Start Mod Manager

---
+++ Rayman2ModManager/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Rayman2ModManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonSaveAndPlay = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonInstall = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.modTab = new System.Windows.Forms.TabPage();
+            this.codesTab = new System.Windows.Forms.TabPage();
+            this.optionsTab = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSaveAndPlay
+            // 
+            this.buttonSaveAndPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSaveAndPlay.Location = new System.Drawing.Point(12, 656);
+            this.buttonSaveAndPlay.Name = "buttonSaveAndPlay";
+            this.buttonSaveAndPlay.Size = new System.Drawing.Size(128, 44);
+            this.buttonSaveAndPlay.TabIndex = 0;
+            this.buttonSaveAndPlay.Text = "Save and play";
+            this.buttonSaveAndPlay.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSave.Location = new System.Drawing.Point(146, 656);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(128, 44);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonInstall
+            // 
+            this.buttonInstall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonInstall.Location = new System.Drawing.Point(280, 656);
+            this.buttonInstall.Name = "buttonInstall";
+            this.buttonInstall.Size = new System.Drawing.Size(128, 44);
+            this.buttonInstall.TabIndex = 2;
+            this.buttonInstall.Text = "Install";
+            this.buttonInstall.UseVisualStyleBackColor = true;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.modTab);
+            this.tabControl.Controls.Add(this.codesTab);
+            this.tabControl.Controls.Add(this.optionsTab);
+            this.tabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl.ItemSize = new System.Drawing.Size(100, 30);
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(580, 650);
+            this.tabControl.TabIndex = 3;
+            // 
+            // modTab
+            // 
+            this.modTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modTab.Location = new System.Drawing.Point(4, 34);
+            this.modTab.Name = "modTab";
+            this.modTab.Padding = new System.Windows.Forms.Padding(3);
+            this.modTab.Size = new System.Drawing.Size(572, 612);
+            this.modTab.TabIndex = 0;
+            this.modTab.Text = "Mods";
+            this.modTab.UseVisualStyleBackColor = true;
+            // 
+            // codesTab
+            // 
+            this.codesTab.Location = new System.Drawing.Point(4, 29);
+            this.codesTab.Name = "codesTab";
+            this.codesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.codesTab.Size = new System.Drawing.Size(572, 617);
+            this.codesTab.TabIndex = 1;
+            this.codesTab.Text = "Codes";
+            this.codesTab.UseVisualStyleBackColor = true;
+            // 
+            // optionsTab
+            // 
+            this.optionsTab.Location = new System.Drawing.Point(4, 29);
+            this.optionsTab.Name = "optionsTab";
+            this.optionsTab.Size = new System.Drawing.Size(572, 617);
+            this.optionsTab.TabIndex = 2;
+            this.optionsTab.Text = "Options";
+            this.optionsTab.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 744);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.buttonInstall);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSaveAndPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(500, 700);
+            this.Name = "MainForm";
+            this.Text = "Rayman2 Mod Manager";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSaveAndPlay;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonInstall;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage modTab;
+        private System.Windows.Forms.TabPage codesTab;
+        private System.Windows.Forms.TabPage optionsTab;
+    }
+}
+
