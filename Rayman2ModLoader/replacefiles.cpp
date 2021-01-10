8dc33baa2aa3673baf7f9105f028b7790bc08072
kellsnc <kellspam@outlook.fr>
2021-01-10 19:08:06 +0100

    Clean up replacefiles

--- Rayman2ModLoader/replacefiles.cpp
+++ Rayman2ModLoader/replacefiles.cpp
@@ -3,36 +3,14 @@
 
 #include <cassert>
 
-#pragma comment (lib, "Dbghelp.lib")
-
-// File replacement map.
-// NOTE: Do NOT mark this as static.
-// MediaFns.cpp needs to access the FileMap.
 FileMap rayman2_fileMap;
 
-/**
-* CreateFileA() wrapper using _ReplaceFile().
-* @param lpFileName
-* @param dwDesiredAccess
-* @param dwShareMode
-* @param lpSecurityAttributes
-* @param dwCreationDisposition
-* @param dwFlagsAndAttributes
-* @param hTemplateFile
-* @return
-*/
 HANDLE WINAPI MyCreateFileA(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) {
 	return CreateFileA(rayman2_fileMap.replaceFile(lpFileName), dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 }
 
 const char* newName = "";
 
-/**
-* fopen() wrapper using _ReplaceFile()
-* @param Filename
-* @param Mode
-* @return
-*/
 __declspec(dllexport) __declspec(naked) FILE* Myfopen(const char* Filename, const char* Mode) {
 	__asm {
 		mov eax, [esp + 4]
@@ -43,17 +21,8 @@ __declspec(dllexport) __declspec(naked) FILE* Myfopen(const char* Filename, cons
 	
 	__asm {
 		mov[esp + 4], eax
-		jmp cs : [0049C1B8h]
+		jmp cs : [0049C1B8h] // return to original fopen in import table
 	}
-}
-
-/**
-* C wrapper to call rayman2_fileMap.replaceFile() from asm.
-* @param lpFileName Filename.
-* @return Replaced filename, or original filename if not replaced by a mod.
-*/
-const char* _ReplaceFile(const char* lpFileName) {
-	return rayman2_fileMap.replaceFile(lpFileName);
 }
 
 void HookProcFunction(const char* dll, const char* funcname, void* newfunc) {
@@ -111,10 +80,7 @@ void HookFileFunctions() {
 	WriteJump((void*)0x45FCB4, Myfopen);
 }
 
-/**
-* Scan for file replacement in a folder
-* @param wstring path
-*/
+// Scan for file replacement in a folder
 void ScanModFolder(const std::wstring str, int loadorder) {
 	if (DirectoryExists(str)) {
 		std::string str2(str.begin(), str.end());
--- Rayman2ModLoader/replacefiles.h
+++ Rayman2ModLoader/replacefiles.h
@@ -2,6 +2,5 @@
 
 extern FileMap rayman2_fileMap;
 
-const char* _ReplaceFile(const char* lpFileName);
 void HookFileFunctions();
 void ScanModFolder(const std::wstring str, int loadorder);
