61c5c12ea2c6c7bf6a90ab3ad48634c14007e170
kellsnc <kellspam@outlook.fr>
2021-01-12 14:40:06 +0100

    Remove dpi auto scaling

--- Rayman2ModManager/Properties/AssemblyInfo.cs
+++ Rayman2ModManager/Properties/AssemblyInfo.cs
@@ -2,35 +2,22 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Rayman2ModManager")]
-[assembly: AssemblyDescription("Manages mod to use with Rayman2ModLoader")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Mod Manager for Rayman2")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rayman2ModManager")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: ComVisible(false)]
 [assembly: Guid("594935f7-3ef4-44e3-ac30-535fbc7157bf")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
